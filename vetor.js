9098ce6b5835fd2e84c29534f1ac854bb88bad77
amanda lacerda <amanda.oliveira.lacerda@escola.pr.gov.br>
2026-03-18 13:04:55 +0000

    certo

---
+++ vetor.js
@@ -0,0 +1,20 @@
+let compras = ["morango", "banana", "pão", "leite", "queijo"];
+
+console.log(compras);
+
+console.log("O Primeiro item da lista é " + compras[0]);
+console.log("O último item da lista é " + compras[4]);
+
+compras[2] = "Macarrão";
+
+console.log(compras[2]); 
+
+compras.push("pepsi");
+compras.push("limão");
+
+compras.pop();
+
+console.log(compras);
+
+console.log(compras.length);
+
