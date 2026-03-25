386a545b64b91a9d9895835ca860d5c11c020a85
amanda lacerda <amanda.oliveira.lacerda@escola.pr.gov.br>
2026-03-25 13:04:03 +0000

    final

---
+++ vetorfor.js
@@ -0,0 +1,13 @@
+const notas = [7.5, 8.2, 9.6, 3.2];
+let soma = 0;
+
+for (i =0; i < nota.length; i++){
+    soma = soma + notas[1];
+
+}
+const media = soma/ nomas.length;
+ console.log("A nota das notas é:" + soma.toFixed(2));
+ console.log("A média do aluno é:" + media.toFixed(2));
+ 
+
+
